d644b169fc5c87d433b6069b74807912f32304f4
Anna Cihlová <anna.cihlova@gmail.com>
2023-06-13 13:55:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -16,10 +16,9 @@ let weatherAPI = {
     const {temp, humidity} = data.main;
     const {speed} = data.wind;
     document.querySelector(".city").innerText = name;
-    document.querySelector(".temperature").innerText = temp + "°C";
+    document.querySelector(".temperature").innerText = temp + " °C";
     document.querySelector(".icon").setAttribute("src", "https://openweathermap.org/img/wn/"+ icon +"@2x.png");
     document.querySelector(".description").innerText = description;
-    document.querySelector(".temperature").innerText = temp;
     document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
     document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
   },
